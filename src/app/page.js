494c1e79060741dcb610ad44068ce3df3eb6c91d
Asif Khan <asif.khan@perficient.com>
2023-12-29 19:51:27 +0530

    Verifying First Deployment on Vercel

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>Sample NextJS Application</div>
+      <div className={styles.description}>
+        First NextJS Application Deployment on Vercel
+      </div>
     </main>
   );
 }
